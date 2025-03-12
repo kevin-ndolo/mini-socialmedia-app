64397e3a73569e7a1ca171c277b3fa963544c94e
kevindev10 <Kevindev1@outlook.com>
2025-03-12 14:41:26 +0300

    Added ability to return number of votes for each post using sqlachemy

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,7 @@
 
 from typing import Optional
 from fastapi import Depends, HTTPException, Response, status, APIRouter
+from sqlalchemy import func
 from .. import models, schemas, oauth2
 from ..database import get_db 
 from sqlalchemy.orm import Session
@@ -13,22 +14,29 @@ router = APIRouter(
 
 
 
-@router.get("/", response_model=list[schemas.Post])
+@router.get("/", response_model=list[schemas.PostOut])
+#@router.get("/")
 async def get_posts(db: Session = Depends(get_db),  user_id: int = Depends(oauth2.get_current_user),
                     limit: int = 50, skip: int = 0, search: Optional[str] = ""):
    
-    posts = db.query(models.Post).filter(models.Post.title.ilike(f"%{search}%")).limit(limit).offset(skip).all()
+    #posts = db.query(models.Post).filter(models.Post.title.ilike(f"%{search}%")).limit(limit).offset(skip).all()
+
+    #results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+
+    posts = db.query(models.Post, func.count(models.Vote.user_id).label("votes")).join(models.Post, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.title.ilike(f"%{search}%")).limit(limit).offset(skip).all()
+
+    #We need to convert the posts to a list of dictionaries to be able to return it as a response to the client 
+    posts = list ( map (lambda x : x._mapping, posts) )
+
+    return posts
     
-    if posts:
-        return posts
-    return {"message": "No posts found"}
 
   
-@router.get("/{post_id}/", response_model=schemas.Post) 
+@router.get("/{post_id}/", response_model=schemas.PostOut) 
 async def get_post(post_id: int, db: Session = Depends(get_db),  current_user: int = Depends(oauth2.get_current_user)):
      
-    post = db.query(models.Post).filter(models.Post.id == post_id).first()
-    
+    #post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    post = db.query(models.Post, func.count(models.Vote.user_id).label("votes")).join(models.Post, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.id == post_id).first()
 
     if not post:
         raise HTTPException(status_code=404, detail=f"Post with id {post_id} was not found")
