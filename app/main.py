8785d644be93a54d1bd611a8d755dc3de068bb9f
kevindev10 <Kevindev1@outlook.com>
2025-03-14 09:00:04 +0300

    Added cors middleware

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 from . import models
 from .database import engine
 from .routers import user, post, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
@@ -18,6 +19,19 @@ db_port = os.environ.get('DATABASE_PORT')
 
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(user.router)
 app.include_router(post.router)
