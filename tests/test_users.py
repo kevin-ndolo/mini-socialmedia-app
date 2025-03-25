3b9b51abec81240e4722c899da03bed5b2aa78c8
kevindev10 <Kevindev1@outlook.com>
2025-03-25 10:35:32 +0300

    Moved test db logic to conftest.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,22 @@
+import pytest
+import jwt
 from app import schemas
-from .database import client, session_fixture
+from app.config import settings
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "steve.jobs@email.com",
+                 "password": "qwerty"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
  
 def test_root(client):
     response = client.get("/")
@@ -15,3 +32,14 @@ def test_create_user(client):
     assert res.status_code == 201
     assert new_user.email == "mike.jones@email.com"
     
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
